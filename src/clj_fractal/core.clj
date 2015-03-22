dab47e7269cb24d09ebfcfddcc1ce38f23a3940a
Jake <jstrength562@gmail.com>
2015-03-22 01:14:21 -0600

    comitting while fix scaling computations

--- src/clj_fractal/core.clj
+++ src/clj_fractal/core.clj
@@ -3,23 +3,37 @@
             [clj-fractal.fractals :refer :all])
   (:gen-class))
 
-(def width 800)
+(def width 600)
 (def height 600)
 (def max-color 100.0)
+
+(defn draw-mandelbrot [data]
+  (println "ypu")
+  ;(println data)
+  (let [pixels (q/pixels)]
+    (doseq [curr (range (* width height))]
+      (let [hue (mod (data curr) max-color)]
+        (aset pixels curr (q/color hue max-color max-color)))))
+  (println "done")
+  (q/update-pixels)
+  (println "done1")
+  )
 
 (defn setup []
   (q/frame-rate 1)
   (q/color-mode :hsb max-color)
-  (q/no-loop))
+  (q/set-state! :dim [4 -4 -4 4])
+  ;(draw-mandelbrot (time (vec (:data (mandelbrot [width height] [(/ width 2) (/ height 2)] @(q/state-atom))))))
+  )
 
 (defn draw []
-  (let [pixels (q/pixels)
-        colors (time (vec (mandelbrot width height)))]
-    ;(println colors)
-    (doseq [curr (range (* width height))]
-      (let [hue (mod (colors curr) max-color)]
-        (aset pixels curr (q/color hue max-color max-color))))
-    (q/update-pixels)))
+  (when (q/mouse-pressed?)
+    (println (q/pmouse-x) " - " (q/pmouse-y))
+    (println (q/mouse-x) " - " (q/mouse-y))
+    (let [brot (mandelbrot [width height] [(/ width 2) (/ height 2)] (:dim @(q/state-atom)))
+          data (time (vec (:data brot)))]
+      (draw-mandelbrot data)
+      (swap! (q/state-atom) assoc :dim (:dim brot)))))
 
 (defn sketch []
   (q/defsketch fractal
--- src/clj_fractal/fractals.clj
+++ src/clj_fractal/fractals.clj
@@ -1,23 +1,22 @@
 (ns clj-fractal.fractals)
 
-(def xMin -2.5)
-(def xMax 1)
-(def yMin -1)
-(def yMax 1)
 (def max-iteration 1000)
 (def bailout 65536)
 
 (defn scale [v a b min max]
   (+ (/ (* (- b a) (- v min)) (- max min)) a))
 
-(defn scale-x [x max]
-  (scale x xMin xMax (* -1 max) max))
+(defn scale-x [x max view-port]
+  (scale x (second view-port) (nth view-port 3) (* -1 max) max))
 
-(defn scale-y [y max]
-  (scale y yMin yMax (* -1 max) max))
+(defn scale-y [y max view-port]
+  (scale y (nth view-port 2) (first view-port) (* -1 max) max))
 
-(defn cartesian [x mid]
+(defn cartesian-x [x mid]
   (- x mid))
+
+(defn cartesian-y [y mid]
+  (- mid y))
 
 (defn process-point [[x0 y0]]
   (loop [x x0
@@ -35,14 +34,29 @@
         iteration)
       (let [newX (+ x0 (- (* x x) (* y y)))
             newY (+ y0 (* 2 x y))]
-        (recur newX newY (inc iteration))))))
+        (if (and (= x newX) (= y newY))
+          max-iteration
+          (recur newX newY (inc iteration)))))))
 
-(defn mandelbrot [width height]
-  (let [mid-width (/ width 2)
-        mid-height (/ height 2)
-        scaled-points (for [y (range height)
-                            x (range width)]
-                        [(scale-x (cartesian x mid-width) mid-width)
-                         (scale-y (cartesian y mid-height) mid-height)])]
-    (pmap process-point scaled-points)))
+(defn calc-view-port [[x y] dim]
+  (let [new-dim (map #(/ % 2) dim)]
+    [(+ (first new-dim) y)
+     (+ (second new-dim) x)
+     (+ (nth new-dim 2) y)
+     (+ (nth new-dim 3) x)]))
+
+(defn mandelbrot [screen-dim new-origin dim]
+  (let [mid-width (/ (first screen-dim) 2)
+        mid-height (/ (second screen-dim) 2)
+        origin-x (scale-x (cartesian-x (first new-origin) mid-width) mid-width dim)
+        origin-y (scale-y (cartesian-y (second new-origin) mid-height) mid-height dim)
+        _ (println [ origin-x origin-y dim])
+        view-port (calc-view-port [origin-x origin-y] dim)
+        _ (println view-port)
+        scaled-points (for [y (range (second screen-dim))
+                            x (range (first screen-dim))]
+                        [(scale-x (cartesian-x x mid-width) mid-width view-port)
+                         (scale-y (cartesian-y y mid-height) mid-height view-port)])]
+    {:data (pmap process-point scaled-points)
+     :dim view-port}))
 
