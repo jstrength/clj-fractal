d0f94db7584a2d224f4ce2fe5f664a3604f3abab
Jake <jstrength562@gmail.com>
2015-03-23 00:58:08 -0600

    zooming working properly now

--- src/clj_fractal/core.clj
+++ src/clj_fractal/core.clj
@@ -4,37 +4,34 @@
   (:gen-class))
 
 (def width 350)
-(def height 200)
+(def height 350)
 (def max-color 100.0)
 
 (defn draw-mandelbrot [data]
-  (println "ypu")
-  ;(println data)
   (let [pixels (q/pixels)]
     (doseq [curr (range (* width height))]
       (let [hue (mod (data curr) max-color)]
         (aset pixels curr (q/color hue max-color max-color)))))
   (println "done")
-  (q/update-pixels)
-  (println "done1")
-  )
+  (q/update-pixels))
 
 (defn setup []
   (q/frame-rate 60)
   (q/color-mode :hsb max-color)
-  (q/set-state! :dim [1.0M -2.5M -1.0M 1.0M]
-                :origin [0.0M 0.0M])
-  ;(draw-mandelbrot (time (vec (:data (mandelbrot [width height] [(/ width 2) (/ height 2)] @(q/state-atom))))))
-  )
+  (q/set-state! :zoom 1
+                :origin [0.0M 0.0M]))
 
 (defn draw []
-  (when (q/mouse-pressed?)
-    (println (q/pmouse-x) " - " (q/pmouse-y))
-    (println (q/mouse-x) " - " (q/mouse-y))
-    (let [brot (mandelbrot [width height] [(q/mouse-x) (q/mouse-y)] (:dim @(q/state-atom)) (:origin @(q/state-atom)))
-          data (time (vec (:data brot)))]
+  (when (or (= 1 (:zoom @(q/state-atom))) (q/mouse-pressed?))
+    (println "clicked! [" (q/mouse-x) (q/mouse-y) "]")
+    (let [new-origin-x (if (q/mouse-pressed?) (q/mouse-x) (/ width 2))
+          new-origin-y (if (q/mouse-pressed?) (q/mouse-y) (/ height 2))
+          brot (time (mandelbrot [width height] [new-origin-x new-origin-y] (:zoom @(q/state-atom)) (:origin @(q/state-atom))))
+          data (vec (:data brot))]
       (draw-mandelbrot data)
-      (swap! (q/state-atom) assoc :dim (:dim brot)))))
+      (swap! (q/state-atom) assoc
+             :origin (:origin brot)
+             :zoom (:zoom brot)))))
 
 (defn sketch []
   (q/defsketch fractal
--- src/clj_fractal/fractals.clj
+++ src/clj_fractal/fractals.clj
@@ -1,14 +1,16 @@
 (ns clj-fractal.fractals
   (:require [clojure.math.numeric-tower :as math]))
 
-(def max-iteration 1000)
-(def bailout 65536M)
+(def max-iteration 100)
+(def bailout 4M)
+(def dim [2.5M -2.5M -2.5M 2.5M])
+(def ZOOM 16)
 
 (defn divide-with-precision [^BigDecimal a ^BigDecimal b]
-  (with-precision 50 (/ a b)))
+  (with-precision 100 (/ a b)))
 
 (defn scale [^BigDecimal v ^BigDecimal a ^BigDecimal b ^BigDecimal min ^BigDecimal max]
-  (+ (divide-with-precision (* (- b a) (- v min)) (- max min)) a))
+  (+ (with-precision 50 (/ (* (- b a) (- v min)) (- max min))) a))
 
 (defn scale-x [^BigDecimal x ^BigDecimal max view-port]
   (scale x (second view-port) (nth view-port 3) (* max -1) max))
@@ -23,13 +25,9 @@
   (- mid y))
 
 (defn process-point [[^BigDecimal x0 ^BigDecimal y0]]
-  ;(println "pp")
-  ;(println x0 " - " y0)
   (loop [x (double x0)
          y (double y0)
          iteration 0]
-    ;(println "loop")
-    ;(println x " - " y)
     (let [x2  (* x x)
           y2  (* y y)]
       (if (or (>= iteration max-iteration)
@@ -48,34 +46,33 @@
             max-iteration
             (recur newX newY (inc iteration))))))))
 
-(defn distance [^BigDecimal x1 ^BigDecimal y1 ^BigDecimal x2 ^BigDecimal y2]
-   (math/sqrt (+ (* (- x2 x1) (- x2 x1)) (* (- y2 y1) (- y2 y1)))))
+(defn calc-view-port [[^BigDecimal x ^BigDecimal y] zoom]
+  [(+ y (divide-with-precision (first dim) zoom))
+   (+ x (divide-with-precision (second dim) zoom))
+   (+ y (divide-with-precision (nth dim 2) zoom))
+   (+ x (divide-with-precision (nth dim 3) zoom))])
 
-(defn calc-view-port [[^BigDecimal x ^BigDecimal y] [^BigDecimal origin-x ^BigDecimal origin-y] dim]
-  (let [new-dim [(divide-with-precision (distance origin-x origin-y origin-x (first dim)) 4.0M)
-                 (divide-with-precision (distance origin-x origin-y (second dim) origin-y) -10.0M)
-                 (divide-with-precision (distance origin-x origin-y origin-x (nth dim 2)) -4.0M)
-                 (divide-with-precision (distance origin-x origin-y (nth dim 3) origin-y) 4.0M)]]
-    [(+ y (first new-dim))
-     (+ x (second new-dim))
-     (+ y (nth new-dim 2))
-     (+ x (nth new-dim 3))]))
-
-(defn mandelbrot [screen-dim new-origin dim origin]
+(defn mandelbrot [screen-dim new-origin zoom origin]
   (let [mid-width (/ (first screen-dim) 2)
         mid-height (/ (second screen-dim) 2)
         prev-origin-x (first origin)
         prev-origin-y (second origin)
-        origin-x (scale-x (cartesian-x (first new-origin) mid-width) mid-width dim)
-        origin-y (scale-y (cartesian-y (second new-origin) mid-height) mid-height dim)
-        _ (println [origin-x origin-y dim])
-        view-port (calc-view-port [origin-x origin-y] [prev-origin-x prev-origin-y] dim)
-        _ (println view-port)
+        origin-x (-> (first new-origin)
+                     (cartesian-x mid-width)
+                     (scale-x mid-width dim)
+                     (divide-with-precision (if (= 1 zoom) 1 (/ zoom ZOOM)))
+                     (+ prev-origin-x))
+        origin-y (-> (second new-origin)
+                     (cartesian-y mid-height)
+                     (scale-y mid-height dim)
+                     (divide-with-precision (if (= 1 zoom) 1 (/ zoom ZOOM)))
+                     (+ prev-origin-y))
+        view-port (calc-view-port [origin-x origin-y] zoom)
         scaled-points (doall (for [y (range (second screen-dim))
                                    x (range (first screen-dim))]
                                [(scale-x (cartesian-x x mid-width) mid-width view-port)
                                 (scale-y (cartesian-y y mid-height) mid-height view-port)]))]
     {:data   (doall (pmap process-point scaled-points))
-     :dim    view-port
+     :zoom   (* ZOOM zoom)
      :origin [origin-x origin-y]}))
 
